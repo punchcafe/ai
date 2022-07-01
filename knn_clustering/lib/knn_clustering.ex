fe2f387a889feb1ab3453848bafb630ec0bb4e2b
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-07-01 21:49:08 +0100

    :tada: add KNN clustering module

---
+++ knn_clustering/lib/knn_clustering.ex
@@ -0,0 +1,18 @@
+defmodule KnnClustering do
+  @moduledoc """
+  Documentation for `KnnClustering`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KnnClustering.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ knn_clustering/test/knn_clustering_test.exs
@@ -0,0 +1,8 @@
+defmodule KnnClusteringTest do
+  use ExUnit.Case
+  doctest KnnClustering
+
+  test "greets the world" do
+    assert KnnClustering.hello() == :world
+  end
+end
